8b2ba4ffe56b365bbab5184fb20c35d685ad136c
zzxx552 <86451335+zzxx552@users.noreply.github.com>
2021-06-26 12:35:49 +0800

    Update diy-1.sh

--- Project/diy-1.sh
+++ Project/diy-1.sh
@@ -1,12 +1,19 @@
-#!/bin/bash
+!/bin/bash
+#
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
-# 说明：
-# 除了第一行的#!/bin/bash不要动，其他的设置，前面带#表示不起作用，不带的表示起作用了（根据你自己需要打开或者关闭）
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
 
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-#源码里面几乎囊括了所有插件，所以基本不需要添加插件了
-
-rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
-#全新的[argon-主题]此主题玩法很多,这里看说明【https://github.com/jerrykuku/luci-theme-argon/blob/18.06/README_ZH.md】
-#用WinSCP连接openwrt，在/www/luci-static/argon里面创建background文件夹（如果本身就有background就不需要创建）来存放jpg png gif格式图片可以自定义登陆界面，gif图片为动态登陆界面
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git jell https://github.com/kenzok8/jell' feeds.conf.default
